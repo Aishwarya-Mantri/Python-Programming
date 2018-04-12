0eaea0f72673ead8249504c453a8e2c88ec00e39
Aish Mantri <aishwaryamantri04@gmail.com>
2018-04-12 16:23:45 +0530

    Find Nearest square number

---
+++ square.py
@@ -0,0 +1,15 @@
+# find the nearest square number of a given positive integer n.
+'''example :nearest_sq(2), 1)
+            nearest_sq(10), 9)
+            nearest_sq(111), 121) '''
+
+import math
+
+def nearest_sq(n):
+  n1=math.floor(n**0.5)  
+  smin=n1**2
+  smax=(n1+1)**2
+  return smin if n-smin < smax-n else smax
+
+print(nearest_sq(111))
+  
