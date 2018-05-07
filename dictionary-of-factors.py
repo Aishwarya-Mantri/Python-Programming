5505911053147983ff3d9ea0e422a00ac78bae3d
Aish Mantri <aishwaryamantri04@gmail.com>
2018-05-07 18:28:39 +0530

    Update dictionary-of-factors.py

--- dictionary-of-factors.py
+++ dictionary-of-factors.py
@@ -3,6 +3,7 @@ The list associated with each key should possess the factors for the number.
 If a number possesses no factors (only 1 and the number itself),
 the list for the key should state 'None'. '''
 
+#solution 1
 def factorsRange(n, m):
     a={}
     for k in range(n,m+1):
@@ -14,6 +15,11 @@ def factorsRange(n, m):
         if a[k]==[]:
             a[k]=['None']
     return a
+print(factorsRange(2,6))
+
+#solution 2
+def factorsRange(n, m):
+    return {k:[v for v in range(2,k) if k%v==0] or ['None'] for k in range(n,m+1)}
 print(factorsRange(2,6))
 
 #Output: {2: ['None'], 3: ['None'], 4: [2], 5: ['None'], 6: [2, 3]}
